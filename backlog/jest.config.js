3ef465d38b16c122ce9b4723660594c15eff280d
chevyb <chevy.banico@sun-asterisk.com>
2022-04-26 13:27:03 +0800

    Update

--- backlog/jest.config.js
+++ backlog/jest.config.js
@@ -4,9 +4,9 @@ module.exports = {
   collectCoverage: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['./src/test'],
+  roots: ['./test'],
   testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   collectCoverageFrom: ['./**/*.js', './**/*.ts'],
-  setupFilesAfterEnv: ['./src/utils/singleton.ts']
+  setupFilesAfterEnv: ['./utils/singleton.ts']
 }
