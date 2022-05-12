367edae797091ea0d4c66c5297cea6260ce48882
Alvilb <alvil.balbuena@sun-asterisk.com>
2022-05-12 14:08:55 +0800

    Remove database hits when testing ProviderRoutes.

--- backlog/test/routes/ProviderRoute.test.ts
+++ backlog/test/routes/ProviderRoute.test.ts
@@ -1,51 +1,39 @@
-import { PrismaClient } from '@prisma/client'
 import request from 'supertest'
 import express from 'express'
-import ProviderRoute from '../../routes/ProviderRoute'
 
-const prisma = new PrismaClient()
+import ProviderRoute from '../../routes/ProviderRoute'
+const Provider = require('../../models/Provider')
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', ProviderRoute)
 
 describe('/providers/:id', () => {
-  let provider: any
-
-  beforeEach(async () => {
-    provider = await prisma.provider.create({
-      data: {
-        user_id: 1,
-        name: 'provider-1',
-        space_key: 'space-key-1',
-        api_key: 'api-key-1'
-      }
-    })
-  })
-
-  afterEach(async () => {
-    const projects = prisma.project.deleteMany()
-    const providers = prisma.provider.deleteMany()
-    await prisma.$transaction([projects, providers])
-    await prisma.$disconnect()
-  })
-
   it('should return 200 and provider details when ID exists', async () => {
-    const response = await request(app).get(`/${provider.id}`)
+    const mockedResponse = {
+      id: 1,
+      user_id: 1,
+      name: 'provider-1',
+      space_key: 'space-key-1',
+      api_key: 'api-key-1',
+      created_at: '2022-05-12T05:42:35.945Z',
+      updated_at: '2022-05-12T05:42:35.946Z',
+      projects: []
+    }
+
+    jest.spyOn(Provider.prototype, 'getProviderById').mockImplementationOnce(() => mockedResponse)
+
+    const response = await request(app).get(`/1111`)
     const parsedResponse = JSON.parse(response.text)
 
     expect(response.statusCode).toBe(200)
-    expect(parsedResponse).toMatchObject({
-      id: 81,
-      ...provider,
-      created_at: parsedResponse.created_at,
-      updated_at: parsedResponse.updated_at
-    })
+    expect(parsedResponse).toMatchObject(mockedResponse)
   })
 
   it('should return 404 and error details when ID do not exist', async () => {
-    const providers = prisma.provider.deleteMany()
-    await prisma.$transaction([providers])
+    jest.spyOn(Provider.prototype, 'getProviderById').mockImplementationOnce(() => null)
+
     const response = await request(app).get('/1111111')
 
     expect(response.statusCode).toBe(404)
